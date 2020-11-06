074940cd9d170265ec5252151f3c44b87b242c17
Khanh Pham <khanhp1406@gmail.com>
2020-11-06 12:43:18 +0700

    update setting.py

--- teko/settings.py
+++ teko/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https:    
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
