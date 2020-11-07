8f6b258c1ac8c8eb7803e89290000fd47f67dd0f
binladen36 <59603151+binladen36@users.noreply.github.com>
2020-11-07 13:58:58 +0700

    ...

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path, include
-from .views import HomeView, LoginView
+from .views import HomeView, LoginView, autocomplete
 from book.views import BookListView
 from book.api.views import BookList
 from book.api.views import BookDetailAPI
@@ -9,6 +9,7 @@ from book.views import SearchBookView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
+    path('autocomplete/', autocomplete, name='autocomplete'),
     path('login/', auth_views.LoginView.as_view(template_name="pages/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name ='logout'),
     path('listbook/', BookListView.as_view(), name ='Books'),
--- home/views.py
+++ home/views.py
@@ -1,8 +1,19 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
 
 
 # Create your views here.
+from book.models import Book
+
+
+def autocomplete(request):
+    print(request.GET)
+    query_original = request.GET.get('term')
+    queryset = Book.objects.filter(name__icontains=query_original)
+    mylist = []
+    mylist += [x.name for x in queryset]
+    return JsonResponse(mylist , safe=False)
 
 class HomeView(View):
     def get(self, request):
