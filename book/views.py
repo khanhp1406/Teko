92485c18455947294b6eee2b9c00b18c3bddb9ca
bamboovennus <bamboovennus@gmail.com>
2020-11-15 13:48:19 +0700

    ??

--- book/views.py
+++ book/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Book
 from django.db.models import Q
 from django.core.paginator import Paginator
+
 # Create your views here.
 
 class BookListView(ListView):
@@ -13,9 +15,21 @@ class BookListView(ListView):
 
 class SearchBookView(ListView):
     model = Book
-    template_name = 'book/listbook.html'
+    template_name = 'book/search_results.html'
+    paginate_by = 30
 
-    def search(self):
-        query = self.request.GET.get('q')
-        result = Book.objects.filter(Q(name__icontains=query) | Q(short_description__icontains=query))
-        return result
+    def get(self, request, *args, **kwargs):
+        q = request.GET.get('q', '')
+        self.results = Book.objects.filter(name__icontains=q)
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(results=self.results, **kwargs)
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book/book_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BookDetailView, self).get_context_data()
+        return context
